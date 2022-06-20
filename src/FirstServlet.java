b9bcb196343b8b92553cb118080ea0c78d05fd30
Valeria Bozhko <whlerwn@yandex.ru>
2022-06-20 15:33:58 +0300

    add app files

---
+++ src/FirstServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+
+        writer.println("<html>");
+        writer.println("<h3>Name list:</h3>");
+        writer.println("<p>Jessica<br>Grace<br>James<br>Albert</p>");
+        writer.println("</html>");
+    }
+}
